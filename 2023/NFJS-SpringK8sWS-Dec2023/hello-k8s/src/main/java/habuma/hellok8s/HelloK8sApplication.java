521f395c543ad8341cc1d48b12f530c350948777
Bob Makowski <tritonpi-git0@yahoo.com>
2024-07-29 16:14:36 -0500

    documented some hacks to 'group' portion of docked image name which is being set to my user name by default

--- 2023/NFJS-SpringK8sWS-Dec2023/hello-k8s/src/main/java/habuma/hellok8s/HelloK8sApplication.java
+++ 2023/NFJS-SpringK8sWS-Dec2023/hello-k8s/src/main/java/habuma/hellok8s/HelloK8sApplication.java
@@ -8,7 +8,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/*
+ * DockerBuild "image" forces all 3 levels of identification associated with docker image being used
+ * @DockerBuild(image="foo/bar:baz")
+ */
+
 @KubernetesApplication(
+        /*
+         * partOf should be picked up from the pom file but it's not exposed that way.
+         * it also is the equivalent this application.property:
+         * #dekorate.kubernetes.partOf=RIOZ
+         * ...
+         * partOf = "RIOZ" ,
+        */
         configMapVolumes = @ConfigMapVolume(
                 configMapName = "greeting-config",
                 volumeName = "greeting-config-volume",
